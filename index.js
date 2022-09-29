eb2a203fd06eded62e8f6a4bba239f8184c94f7f
Matthew <mtgreff5@gmail.com>
2022-09-29 18:30:25 -0400

    commit tree 3997504a005730dca8e8a40a98d690603296ba79

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function saturdayFun(funActivity = 'roller-skate'){
+    return `This Saturday, I want to ${funActivity}!`
+}
+
+function mondayWork(boringChore = 'go to the office'){
+    return `This Monday, I will ${boringChore}.`
+}
